fc5470d5d68c27ccbead34345703f2af5dc3e798
gkatjddl <alswjddn1002@gmail.com>
2024-05-18 11:24:57 +0900

    2024-05-18

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,28 +1,30 @@
-import { createContext, useState } from 'react'
-import './context.css'
+import { createContext, useState } from "react";
 
-export const GlobalContext = createContext(null);
+export const GlobalContext = createContext(null);     // useContext로 가져다 쓸 수 있음
+console.log('context render');
 
-// 리덕스 대신에 전역적으로 사용할 state (index.js 에서 App컴포넌트를 감싸줘야함)
+// 리덕스 대신에 전역적으로 사용할 state (index.js에서 App 컴포넌트를 감싸줘야함 (리덕스와 유사) )
 export default function GlobalState({children})
 {
-  // 검색값
-  let[searchParam, setSearchParam] = useState("");
-  // 음식리스트
-  let[foodList, setFoodList] = useState([]);
-  // 음식 상세 데이터
-  let[foodDetailList, setFoodDtailList] = useState(null);
-  // 즐겨찾기 등록 리스트
-  let[favoritesList,setFavoritesList] = useState([]);
+  // 검색값 state
+  let [searchParam, setSearchParam] = useState("");
+  // 음식리스트 state
+  let [foodList, setFoodList] = useState([]);
+  // 음식 상세데이터 state
+  let [foodDetailData, setFoodDetailData] = useState(null);
+  // 즐겨찾기 등록 리스트 state
+  let [favoritesList, setFavoritesList] = useState([]);
 
   // 제공할 함수
   // 검색을 하면 검색명으로 get요청
-  async function hSubmit(event){
-    try
+  async function hSubmit(event) {
+    event.preventDefault();     // 부모까지 이벤트가 버블링 되는 것을 막는다(전달되는것 막음)
+    try     // 실패할수도 있는 함수는 try{} catch(e){} 로 감싸준다
     {
-      const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`);
-      const information = await res.json();    // REST API는 JSON문자열로전달하기 떄문에 사용할수있는 자료형으로 변경
-      console.log(information)
+      const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`);     // get요청 (REST API의 GET요청)
+      const information = await res.json();      // REST API는 JSON문자열로 전달하니깐 사용할 수 있는 자료형으로 변경
+
+      console.log(information);
       if(information?.data.recipes){
         setFoodList(information?.data.recipes);
         setSearchParam('');
@@ -33,7 +35,7 @@ export default function GlobalState({children})
   }
 
   return(
-    <GlobalContext.Provider value={{searchParam,setSearchParam,foodList,setFoodList,foodDetailList,setFoodDtailList,favoritesList,setFavoritesList,hSubmit}}>
+    <GlobalContext.Provider value={{searchParam, setSearchParam, hSubmit, foodList, setFoodList, foodDetailData, setFoodDetailData, favoritesList}}>
       {children}
     </GlobalContext.Provider>
   )
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,6 @@
-import { useContext } from 'react'
-import './home.css'
-import { GlobalContext } from '../../context/context'
+import { useContext } from 'react';
+import './home.css';
+import { GlobalContext } from '../../context/context';
 import DetailItem from '../../components/detail-item/detail-item';
 
 export default function Home()
@@ -10,21 +10,22 @@ export default function Home()
   return(
     <div className='home-container'>
       {
-        foodList && foodList.lengh > 0 ?(
+        foodList && foodList.length > 0 ? (
           // 데이터가 있을때
-          foodList.map((food,idx)=>{
+          foodList.map((food, idx)=>{
             return(
-              <DetailItem item={food} key={idx} />
+              // map에서 컴포넌트를 생성할 땐 첫번째 인자를 전달
+              <DetailItem item={food} key={idx}/>
             )
           })
-        ):(
+        ) 
+        : (
           // 데이터가 없을때
           <div className='no-item'>
-            <p>검색하세요.pizza,banana.....</p>
-            <a href='https://forkify-api.herokuapp.com/phrases.html' target='_blank'>
+            <p>검색하세요. pizza, banana, apple ...</p>
+            <a href="https://forkify-api.herokuapp.com/phrases.html" target='_blank'>
               <button className='guide-btn'>검색 항목 안내</button>
             </a>
-            
           </div>
         )
       }
